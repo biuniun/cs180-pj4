940bdfb889582df2d61accbd5460de8383b3ded1
Niftiy <125300079+Niftiy@users.noreply.github.com>
2023-04-06 20:41:51 -0400

    Update Seller.java

--- src/users/Seller.java
+++ src/users/Seller.java
@@ -1,5 +1,8 @@
 package users;
 
-public class Seller {
-	
+public class Seller extends User {    
+    public Seller(int id, String username, String password, String userType, ArrayList<User> blockedUsers)
+    {
+        super(id, username, password, userType, blockedUsers);
+    }
 }
